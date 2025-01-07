164a60e235a1414b6c1fc00d7578561bc278ea97
Kay Schnyder <schnyderkay@gmail.com>
2025-01-07 10:33:03 +0100

    remove backend and add docker

--- backend/src/main/java/com/example/demo/domain/storage/StorageRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.domain.storage;
-
-import com.example.demo.core.generic.AbstractRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface StorageRepository extends AbstractRepository<Storage> {
-}
